0a3f89a016b662e11de8539d5b2fdfc5243d35aa
adiyatmubarak <adiyatmubarak@gmail.com>
2016-03-21 17:31:21 +0700

    handle post method wsgi

--- learn-python/wsgi_demo.py
+++ learn-python/wsgi_demo.py
@@ -1,9 +1,32 @@
+import cgi
 from wsgiref.simple_server import make_server
 
 
 def app(environ, start_response):
+    html = b"""
+    <html>
+        <head>
+            <title>Hello User!</title>
+        </head>
+        <body>
+            <form method="post">
+                <label>Hello</label>
+                <input type="text" name="name">
+                <input type="submit" value="Go">
+            </form>
+        </body>
+    </html>
+    """
+    if environ['REQUEST_METHOD'] == 'POST':
+        post_env = environ.copy()
+        post_env['QUERY_STRING'] = ''
+        import ipdb; ipdb.set_trace()
+        post = cgi.FieldStorage(fp=environ['wsgi.input'], environ=post_env,
+                                keep_blank_values=True)
+        html = b'Hello, ' + post['name'].value + '!'
+
     start_response('200 OK', [('Content-Type', 'text/html')])
-    return [b'Hello world, wsgi!']
+    return [html]
 
 if __name__ == '__main__':
     try:
