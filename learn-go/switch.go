73775133fe4cb70e39dcf0ee640c3ccf126f5f42
adiyatmubarak <adiyatmubarak@gmail.com>
2016-06-10 11:15:11 +0700

    add more

--- learn-go/switch.go
+++ learn-go/switch.go
@@ -1,1 +1,25 @@
-package learn_go
+package main
+
+import "fmt"
+import "time"
+
+func main() {
+	n := 2
+	fmt.Print("Write ", n, " as ")
+	switch n {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	}
+
+	fmt.Println(time.Now().Weekday())
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's weekend!")
+	default:
+		fmt.Println("It's weekday!")
+	}
+}
