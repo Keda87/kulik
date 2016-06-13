4a3d2a89a2047518265c2434e64b36c1ee5c4488
adiyatmubarak <adiyatmubarak@gmail.com>
2016-06-13 16:31:18 +0700

    data structure go

--- learn-go/maps.go
+++ learn-go/maps.go
@@ -1,1 +1,23 @@
-package learn_go
+package main
+
+import "fmt"
+
+func main() {
+	m := make(map[string]int)
+	m["k1"] = 7
+	m["k2"] = 13
+	fmt.Println("map:", m)
+
+	v1 := m["k1"]
+	fmt.Println("k1", v1)
+	fmt.Println(len(m))
+
+	delete(m, "k2")
+	fmt.Println("map:", m)
+
+	_, prs := m["k2"]
+	fmt.Println("prs:", prs)
+
+	n := map[string]int{"foo": 1, "bar": 2}
+	fmt.Println("map:", n)
+}
