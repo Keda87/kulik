070f55a7a58d312e3786866ebdf37094c3575391
Keda87 <adiyatmubarak@gmail.com>
2016-09-04 11:32:18 +0700

    feat: Add function example using list comprehension

--- learn-haskell/src/FunctionDemo.hs
+++ learn-haskell/src/FunctionDemo.hs
@@ -3,3 +3,8 @@ doubleMe x = x + x
 doubleUs x y = doubleMe x + doubleMe y
 
 doubleSmallNumber x = (if x > 100 then x else x*2)
+
+-- Function to remove uppercase with list comperhension.
+removeUpperCase arg = [x | x <- arg, x `elem` ['A'..'Z']]
+
+getVowel arg = [x | x <- arg, x `elem` ['a', 'A', 'i', 'I', 'u', 'U', 'e', 'E', 'o', 'O']]
