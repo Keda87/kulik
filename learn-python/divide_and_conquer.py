f1b484a0e2d07feb3c4870440a67eff3980192b0
Keda87 <adiyatmubarak@gmail.com>
2016-09-07 07:31:38 +0700

    Add more D&Q example

--- learn-python/divide_and_conquer.py
+++ learn-python/divide_and_conquer.py
@@ -28,7 +28,28 @@ def max_and_min_good(lst):
     min2, max2 = max_and_min_good(lst[mid:])
     return (min1 if min1 < min2 else min2, max1 if max1 > max2 else max2)
 
+
+def sums_bad(lst):
+    """Find total list value with O(n)."""
+    total = 0
+    for i in lst:
+        total += i
+    return total
+
+
+def sums_good(lst):
+    """Find total list value with O(log n)."""
+    if len(lst) == 1:
+        return lst[0]
+    mid = len(lst) // 2
+    left = sums_good(lst[:mid])
+    right = sums_good(lst[mid:])
+    return left + right
+
+
 if __name__ == '__main__':
     ar = [39, 4, 2, 6, 7 ,4, 32, 100, 24, 22, 12, 47, -1]
-    print max_and_min_bad(ar)
-    print max_and_min_good(ar)
+    print 'Max and Min O(n):', max_and_min_bad(ar)
+    print 'Max and Min O(log n):', max_and_min_good(ar)
+    print 'Sum O(n):', sums_bad(ar)
+    print 'Sum O(log n):', sums_good(ar)
