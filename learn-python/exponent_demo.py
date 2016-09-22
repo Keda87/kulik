b88cbb01acfca32ad7ef25ca137e9afe86673a7d
Keda87 <adiyatmubarak@gmail.com>
2016-09-22 19:12:00 +0700

    exponentiation with square example

---
+++ learn-python/exponent_demo.py
@@ -0,0 +1,26 @@
+
+def bad_exponent(a, b):
+    """Compute exponentiation a^b with O(n)."""
+    result = 1
+    for i in range(b):
+        result *= a
+    return result
+
+
+def good_exponent(a, b):
+    """Compute exponentiation a^b with exponentiation
+    with squaring algorithm that has O(log(n) + log(n)).
+    ref: https://simple.wikipedia.org/wiki/Exponentiation_by_squaring"""
+    if b == 1:
+        return a
+    if b == 2:
+        return a * a
+    if b % 2 == 0:
+        return good_exponent(good_exponent(a, b / 2), 2)
+    else:
+        return a * good_exponent(good_exponent(a, (b - 1) / 2), 2)
+
+
+if __name__ == '__main__':
+    print bad_exponent(2, 999999)  # 23 secs
+    print good_exponent(2, 999999) # 3 secs
