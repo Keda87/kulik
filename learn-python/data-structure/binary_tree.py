e0c040453d5a4e8ad24c4932e624822f87ba98a9
Keda87 <adiyatmubarak@gmail.com>
2016-10-04 12:21:57 +0700

    search, find max and min binary tree

--- learn-python/data-structure/binary_tree.py
+++ learn-python/data-structure/binary_tree.py
@@ -5,11 +5,40 @@ class Node(object):
         self.right = None
         self.data = data
 
-def inorder_tree_walk(x):
-    if x is not None:
-        inorder_tree_walk(x.left)
-        print x.data,
-        inorder_tree_walk(x.right)
+
+class BinaryTreeDemo(object):
+
+    def __init__(self, node=None):
+        self.root = node
+
+    def inorder_tree_walk(self, node=None):
+        if node is not None:
+            self.inorder_tree_walk(node.left)
+            print node.data,
+            self.inorder_tree_walk(node.right)
+
+    def search(self, current_node=None, data=None):
+        if current_node is None:
+            current_node = self.root
+        while current_node is not None and current_node.data != data:
+            if data < current_node.data:
+                current_node = current_node.left
+            else:
+                current_node = current_node.right
+        return current_node
+
+    def min(self):
+        node = self.root
+        while node.left is not None:
+            node = node.left
+        return node
+
+    def max(self):
+        node = self.root
+        while node.right is not None:
+            node = node.right
+        return node
+
 
 if __name__ == '__main__':
     """
@@ -21,12 +50,15 @@ if __name__ == '__main__':
         2   5   8
     """
     bt = Node(6)
-
     bt.left = Node(5)
     bt.left.left = Node(2)
     bt.left.right = Node(5)
-
     bt.right = Node(7)
     bt.right.right = Node(8)
 
-    inorder_tree_walk(bt)
+    bin_tree = BinaryTreeDemo(bt)
+    bin_tree.inorder_tree_walk(bin_tree.root)
+    print
+    print bin_tree.search(data=7)
+    print 'Max: {}'.format(bin_tree.max().data)
+    print 'Min: {}'.format(bin_tree.min().data)
