64c472c67fc7c7d09a79b0c52b3ee3b5c816ca6f
Keda87 <adiyatmubarak@gmail.com>
2016-10-09 11:49:03 +0700

    secondary constructor in kotlin class

--- learn-kotlin/src/hello/Person.kt
+++ learn-kotlin/src/hello/Person.kt
@@ -3,9 +3,19 @@ package hello
 /**
  * Created by adiyatmubarak on 10/9/16.
  */
-class Person(var name: String, var age: Int, var college: String?)
+class Person(var name: String, var age: Int, var college: String?) {
+
+    var email = ""
+
+    constructor(name: String, age: Int, college: String?, email: String): this(name, age, college) {
+        this.email = email
+    }
+}
 
 fun main(args: Array<String>) {
     val jake = Person("Jake Hill", 24, "Stanford")
-    println(jake.name)
+    println(jake.name + " - " + jake.email)
+
+    val thomas = Person("Thomas Matthew", 18, "MIT", "thomas@gmail.com")
+    println(thomas.name + " - " + thomas.email)
 }
