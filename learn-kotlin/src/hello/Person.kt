95f6ebf9166bca60add8b7569d9c4b974aad1248
Keda87 <adiyatmubarak@gmail.com>
2016-10-09 11:40:09 +0700

    constructor in kotlin

--- learn-kotlin/src/hello/Person.kt
+++ learn-kotlin/src/hello/Person.kt
@@ -3,16 +3,9 @@ package hello
 /**
  * Created by adiyatmubarak on 10/9/16.
  */
-class Person {
-    var name: String = ""
-    var age: Int = 0
-    var college: String? = null
-}
+class Person(var name: String, var age: Int, var college: String?)
 
 fun main(args: Array<String>) {
-    val jake = Person()
-    jake.name = "Jake Hill"
-    jake.age = 24
-    jake.college = "Telkom University"
+    val jake = Person("Jake Hill", 24, "Stanford")
     println(jake.name)
 }
