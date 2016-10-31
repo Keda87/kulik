ce7df8bf868e03bdd10ea200198c29c5e3df7eda
Keda87 <adiyatmubarak@gmail.com>
2016-11-01 06:18:58 +0700

    serving web content spring boot example

---
+++ learn-java/learn-spring/spring-boot-demo/src/main/java/hello/web/GreetingController.java
@@ -0,0 +1,21 @@
+package hello.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by adiyatmubarak on 11/1/16.
+ */
+@Controller
+public class GreetingController {
+
+    @RequestMapping(value = "/greeting", method = RequestMethod.GET)
+    public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World!") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
